82b24418f0fd1ac836455a797a2ce780a87fae70
Sagar Saha <59755509+SagarSaha01@users.noreply.github.com>
2022-08-27 16:04:09 -0400

    save user information and retrieve them

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const app = express()
 const session = require("express-session")
 const passport = require("passport")
 const passportLocalMongoose = require("passport-local-mongoose")
+const multer = require('multer');
+const { GridFsStorage } = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override');
+var error = "none", showData = []
 
+//  Set Up
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.use(session({
@@ -18,8 +24,38 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(passport.initialize())
 app.use(passport.session())
-mongoose.connect("mongodb://localhost:27017/ProjectX", { useNewUrlParser: true })
+app.use(methodOverride('_method'));
+mongoose.connect("mongodb://localhost:27017/healthX", {useNewUrlParser: true})
+const conn = mongoose.createConnection("mongodb://localhost:27017/healthX");
 
+let gfs, gridfsBucket
+conn.once('open', () => {
+  gridfsBucket = new mongoose.mongo.GridFSBucket(conn.db, {
+    bucketName: 'fileData'
+  });
+
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection('fileData');
+})
+
+// Multer Storage
+const storage = new GridFsStorage({
+  url: "mongodb://localhost:27017/healthX",
+  file: (req, file) => {
+
+    return new Promise((resolve, reject) => {
+      const fileInfo = {
+        filename: file.originalname,
+        metadata: showData[0].username,
+        bucketName: 'fileData'
+      };
+      resolve(fileInfo);
+    });
+  }
+});
+const upload = multer({ storage });
+
+// MongoDB Schemas
 const dataSchema = new mongoose.Schema ({
   firstName: String,
   lastName: String,
@@ -47,6 +83,7 @@ dataSchema.plugin(passportLocalMongoose)
 const userData = new mongoose.model("data", dataSchema)
 const profileData = new mongoose.model("profileData", profileSchema)
 
+// Passport Js Set Up
 passport.use(userData.createStrategy());
 passport.serializeUser(function(user, done) {
   done(null, user.id);
@@ -58,6 +95,8 @@ passport.deserializeUser(function(id, done) {
   });
 });
 
+
+// Get Requests
 app.get('/', (req, res) => {
   res.render("index")
 })
@@ -73,19 +112,17 @@ app.get("/profile", (req, res) => {
 
   if (req.isAuthenticated()) {
 
-    
-
+    gfs.files.find({metadata: showData[0].username}).toArray(((err, files) => {
       res.render("profile", {
         profileData: showData,
-
+        files: files
       })
-
+    }))
     
   } else {
     res.redirect("/error401")
   }
 })
-
 
 app.get('/editProfile', (req, res) => {
   if (req.isAuthenticated()) {
@@ -105,12 +142,31 @@ app.get("/signOut", (req, res) => {
   });
 })
 
+app.get("/files/:filename", (req, res) => {
+
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+
+    // Check if file
+    if (!file || file.length === 0) {
+      res.send("Fatal Error: No files Found")
+    }
+
+    const readStream = gridfsBucket.openDownloadStream(file._id);
+    readStream.pipe(res);
+
+  });
+})
 
 app.get("/error401", (req, res) => {
   res.render("error401")
 })
 
+app.get("/:query", (req, res) => {
+  res.render("error404")
+})
 
+ 
+// Post Requests
 app.post("/signUp", (req, res) => {
 
   userData.register({ username: req.body.username }, req.body.password, function (err, user) {
@@ -166,8 +222,47 @@ app.post("/signIn", (req, res) => {
 
 })   
 
+app.post('/editProfile', (req, res) => {
+  var element = {
+    fullName: req.body.fullName,
+    email: req.body.email,
+    username: showData[0].username,
+    phoneNumber: req.body.num,
+    address: req.body.address,
+    age: req.body.age,
+    height: req.body.height,
+    weight: req.body.weight,
+    cardInfo: req.body.cardInfo,
+    hospital: req.body.nearestHospital,
+    lastVisited: req.body.lastVisited,
+    upcoming: req.body.upcoming
+  }
 
+  profileData.replaceOne({ username: showData[0].username }, element, function (err, res) {
+    if (err) {
+      console.log(err);
+    }
+  })
+  showData[0] = element
+  res.redirect("/profile")
+
+})
+
+app.post('/upload', upload.single('uploadedFile'), (req, res) => {
+  console.log(req.file);
+  res.redirect('/profile')
+});
+
+app.post('/files/:id', (req, res) => {
+  gfs.remove({ _id: req.params.id }, function (err) {
+    if (err) return handleError(err);
+    console.log('success');
+    res.redirect("/profile")
+  });
+});
+
+
+// Start App
 app.listen(3000, () => {
   console.log("3000");
 })
-
